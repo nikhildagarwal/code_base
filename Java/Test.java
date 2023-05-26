e56960f7679a270a32bffc61334135d15b3acd37
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-05-26 17:35:43 -0400

    Full Pair implementation

--- Java/Test.java
+++ Java/Test.java
@@ -6,23 +6,11 @@ import Java.data_structures.List;
 public class Test {
 
     public static void main(String[] args){
-        Pair<Pair<Integer,String>,Pair<List<Integer>,Boolean>> p1 = new Pair<>();
-        p1.setFirst(new Pair<>(6,"sup"));
-        List<Integer> l1 = new List<>();
-        l1.add(6);
-        l1.add(2);
-        l1.add(3);
-        l1.add(2);
-        l1.add(7);
-        p1.setSecond(new Pair<>(l1,false));
-        Pair<Pair<Integer,String>,Pair<List<Integer>,Boolean>> p2 = new Pair<>(p1);
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p2.equals(p1));
-        p2.setFirst(new Pair<>(6,"hi"));
-        System.out.println(p1);
-        System.out.println(p2);
-        System.out.println(p2.equals(p1));
+        Pair<String,Integer> pair = new Pair<>("My Name is Nikhil",21);
+        System.out.println(pair);
+        int code = pair.hashCode();
+        System.out.println(code);
+
     }
 
 }

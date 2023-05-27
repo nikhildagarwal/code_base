518c574443734891fe01cc0c14a56440f4993726
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-05-26 21:42:43 -0400

    full stack implementation

--- Java/Test.java
+++ Java/Test.java
@@ -6,9 +6,10 @@ import Java.structures.*;
 public class Test {
 
     public static void main(String[] args){
-        Stack<Integer> stack = new Stack<>();
-        for(int i = 0;i<1000000000;i++){
-            stack.push(i);
+        Stack<Integer> stack = new Stack<>(10000000);
+        for(int i = 0;i<100000;i++){
+                stack.push(3);
+
         }
         System.out.println(stack);
     }
--- Java/structures/Stack.java
+++ Java/structures/Stack.java
@@ -8,18 +8,29 @@ public class Stack<T> {
     private int size;
     private int capacity;
 
+    /**
+     * Default constructor for Stack Object;
+     */
     public Stack(){
         this.capacity = INIT_CAPACITY;
         this.stack = (T[]) new Object[capacity];
         this.size = 0;
     }
 
+    /**
+     * Creates Stack Object with a specified init capacity.
+     * @param capacity
+     */
     public Stack(int capacity){
         this.capacity = capacity;
         this.stack = (T[]) new Object[capacity];
         this.size = 0;
     }
 
+    /**
+     * Push a value onto the top of our stack.
+     * @param value Generic object to push
+     */
     public void push(T value){
         if(size >= stack.length){
             grow();
@@ -28,6 +39,10 @@ public class Stack<T> {
         size++;
     }
 
+    /**
+     * get the value at the top of our stack WITHOUT removing it
+     * @return generic value at the top stack
+     */
     public T peek(){
         if(size == 0){
             throw new IndexOutOfBoundsException("Stack is Empty!");
@@ -35,6 +50,10 @@ public class Stack<T> {
         return stack[size-1];
     }
 
+    /**
+     * get the top value of our stack and remove it from the stack
+     * @return
+     */
     public T pop(){
         if(size == 0){
             throw new IndexOutOfBoundsException("Stack is Empty!");
@@ -47,23 +66,43 @@ public class Stack<T> {
         return val;
     }
 
+    /**
+     * Checks to see how much capacity is available before we resize the stack
+     * For developer use (maximize efficiency)
+     * @return int number of null spaces in stack
+     */
     public int capacityAvailable(){
         return stack.length-size;
     }
 
+    /**
+     * size of our stack (number of elements in the stack)
+     * @return
+     */
     public int size(){
         return size;
     }
 
+    /**
+     * method to check and see if our stack is empty or not
+     * @return true if stack has no elements, false otherwise
+     */
     public boolean isEmpty(){
         return size == 0;
     }
 
+    /**
+     * clear our stack and reset it.
+     */
     public void clear(){
         this.stack = (T[]) new Object[capacity];
         this.size = 0;
     }
 
+    /**
+     * reverse our stack in place.
+     * The bottom now becomes the top
+     */
     public void reverse(){
         int pR = size-1;
         for(int i = 0;i<size/2;i++){
@@ -74,6 +113,11 @@ public class Stack<T> {
         }
     }
 
+    /**
+     * Check to see if the stack object contains a certain element
+     * @param value generic value T we want to check for
+     * @return true if we find that value, false otherwise
+     */
     public boolean contains(T value){
         for(int i = 0;i<size;i++){
             if(stack[i].equals(value)){
@@ -83,6 +127,11 @@ public class Stack<T> {
         return false;
     }
 
+    /**
+     * return the number of time an element is found in our stack object
+     * @param value generic value T we want to check for
+     * @return int number of times that element is present in our stack
+     */
     public int count(T value){
         int ans = 0;
         for(int i = 0;i<size;i++){
@@ -93,6 +142,12 @@ public class Stack<T> {
         return ans;
     }
 
+    /**
+     * Override method for equals in Java.Object class.
+     * checks if two Stack objects are equal to each other.
+     * @param obj Stack Object we want to check
+     * @return true if equal, false otherwise
+     */
     @Override
     public boolean equals(Object obj){
         Stack<T> s = (Stack<T>) obj;
@@ -110,6 +165,11 @@ public class Stack<T> {
         return true;
     }
 
+    /**
+     * Coverts Stack Object to a printable string.
+     * Easier visualization.
+     * @return String of format --> { |val1 ]val2 ]val3 ]... ]valn }
+     */
     @Override
     public String toString(){
         String ans = "{";
@@ -128,10 +188,17 @@ public class Stack<T> {
         return capacity;
     }
 
+    /**
+     * getter method for T[] stack array
+     * @return T[] stack array
+     */
     private T[] getStack(){
         return stack;
     }
 
+    /**
+     * helper method to grow stack dynamically
+     */
     private void grow(){
         T[] newS = (T[]) new Object[stack.length+capacity];
         for(int i = 0;i<size;i++){
@@ -140,6 +207,9 @@ public class Stack<T> {
         this.stack = newS;
     }
 
+    /**
+     * helper method to shrink our stack
+     */
     private void shrink(){
         T[] newS = (T[]) new Object[stack.length-capacity];
         for(int i = 0;i<size;i++){

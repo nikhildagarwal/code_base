39fbd13f9332249579489d1f97bf91cac771cf27
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-06-02 15:03:08 -0400

    graph Node remove Connection implementation

--- Java/data_structures/node_implementation/GraphNode.java
+++ Java/data_structures/node_implementation/GraphNode.java
@@ -4,6 +4,7 @@ public class GraphNode<T> {
 
     private T val;
     private Node<GraphNode<T>> connections;
+    private int numberOfConnections;
 
     public GraphNode(){
         this.val = null;
@@ -15,20 +16,63 @@ public class GraphNode<T> {
         this.connections = null;
     }
 
-    public void addConnection(GraphNode<T> node){
+    /**
+     * Adds connection from this node to a given node
+     * @param node GraphNode to connect to
+     * @return true if connections was added, false if connection already exists.
+     */
+    public boolean addConnection(GraphNode<T> node){
         if(!searchAndFound(node)){
             Node<GraphNode<T>> start = new Node<>(node);
             start.next(connections);
             connections = start;
-            Node<GraphNode<T>> newStart = new Node<>(this);
-            newStart.next(node.connections);
-            node.connections = newStart;
+            if(node!=this){
+                Node<GraphNode<T>> newStart = new Node<>(this);
+                newStart.next(node.connections);
+                node.connections = newStart;
+                node.numberOfConnections++;
+            }
+            this.numberOfConnections++;
+            return true;
         }
+        return false;
+    }
+
+    public int numConnections(){
+        return numberOfConnections;
+    }
+
+    public boolean removeConnection(GraphNode<T> gnode){
+        if(connections == null){
+            return false;
+        }
+        if(connections.val()==gnode){
+            connections = connections.next();
+            numberOfConnections--;
+            if(this!=gnode){
+                cleanup(gnode);
+            }
+            return true;
+        }
+        Node<GraphNode<T>> start = connections;
+        while(start.next()!=null){
+            if(start.next().val()==gnode){
+                start.next(start.next().next());
+                numberOfConnections--;
+                if(this!=gnode){
+                    cleanup(gnode);
+                }
+                return true;
+            }
+            start = start.next();
+        }
+        return false;
     }
 
     @Override
     public String toString() {
         String ans = "Node: "+val+"\n";
+        ans += "Count: "+numberOfConnections+"\n";
         ans += "Connections: [";
         Node<GraphNode<T>> start = connections;
         while(start!=null){
@@ -39,6 +83,22 @@ public class GraphNode<T> {
             start = start.next();
         }
         return ans + "]\n---------------";
+    }
+
+    private void cleanup(GraphNode<T> gnode){
+        if(gnode.connections.val()==this){
+            gnode.connections = gnode.connections.next();
+            gnode.numberOfConnections--;
+        }else{
+            Node<GraphNode<T>> gnodeStart = gnode.connections;
+            while(gnodeStart.next()!=null){
+                if(gnodeStart.next().val()==this){
+                    gnodeStart.next(gnodeStart.next().next());
+                    gnode.numberOfConnections--;
+                }
+                gnodeStart = gnodeStart.next();
+            }
+        }
     }
 
     private boolean searchAndFound(GraphNode<T> node){
@@ -53,15 +113,19 @@ public class GraphNode<T> {
     }
 
     public static void main(String[] args){
-        GraphNode<String> LAX = new GraphNode<>("LAX");
-        GraphNode<String> EWR = new GraphNode<>("EWR");
         GraphNode<String> JFK = new GraphNode<>("JFK");
-        LAX.addConnection(EWR);
-        LAX.addConnection(JFK);
+        GraphNode<String> LAX = new GraphNode<>("LAX");
+        GraphNode<String> DAL = new GraphNode<>("DAL");
+        GraphNode<String> EWR = new GraphNode<>("EWR");
         JFK.addConnection(LAX);
-        System.out.println(LAX);
-        System.out.println(EWR);
+        JFK.addConnection(DAL);
+        JFK.addConnection(EWR);
+
+        System.out.println(DAL.removeConnection(JFK));
         System.out.println(JFK);
+        System.out.println(LAX);
+        System.out.println(DAL);
+        System.out.println(EWR);
     }
 
 }

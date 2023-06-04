4e3a386293126905bc160fdd9efe7a68fb5100b6
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-06-03 21:05:06 -0400

    graphNode imp and comments

--- Java/data_structures/node_implementation/GraphNode.java
+++ Java/data_structures/node_implementation/GraphNode.java
@@ -1,19 +1,59 @@
 package Java.data_structures.node_implementation;
 
+/**
+ * Java Implementation of Graph Node Object (Build graphs)
+ * Build undirected graphs using this Data structure. Each Node contains a value and linkedList of Nodes that this Node points to.
+ * If a node adds a connections to another node, the connections is bidirectional.
+ * Likewise, if a connection is removed in one node, the connections is severed from both ends.
+ * Init as -->  GraphNode<T> graphNode = new GraphNode<>(); //Null node
+ *              GraphNode<T> graphNode = new GraphNode<>(val); //Init with value val
+ * Methods -->  boolean addConnection(GraphNode<T> node);
+ *              boolean removeConnection(GraphNode<T> node);
+ *              void val(T value);
+ *              T val();
+ *              int numConnections();
+ *              GraphNode<T>[] getConnections();
+ *              @Override String toString();
+ * @param <T> Class Limiter
+ * @author Nikhil Daehee Agarwal
+ */
 public class GraphNode<T> {
 
     private T val;
     private Node<GraphNode<T>> connections;
     private int numberOfConnections;
 
+    /**
+     * Default Constructor for GraphNode Object. Null Node
+     */
     public GraphNode(){
         this.val = null;
         this.connections = null;
     }
 
+    /**
+     * Init GraphNode Object with a given value
+     * @param val Value of Node
+     */
     public GraphNode(T val){
         this.val = val;
         this.connections = null;
+    }
+
+    /**
+     * getter method for the value stored in this current node.
+     * @return T value
+     */
+    public T val(){
+        return val;
+    }
+
+    /**
+     * setter method to set the value of this node to given value
+     * @param value T object
+     */
+    public void val(T value){
+        this.val = value;
     }
 
     /**
@@ -38,10 +78,35 @@ public class GraphNode<T> {
         return false;
     }
 
+    /**
+     * getter method for an array of nodes that this node points to.
+     * @return GraphNode<T>[] containing array of pointers to connection nodes.
+     */
+    public GraphNode<T>[] getConnections(){
+        GraphNode<T>[] array = new GraphNode[numberOfConnections];
+        Node<GraphNode<T>> start = connections;
+        int i = 0;
+        while(start!=null){
+            array[i] = start.val();
+            i++;
+            start = start.next();
+        }
+        return array;
+    }
+
+    /**
+     * getter method for the total number of nodes this node has connections to.
+     * @return int number of connections
+     */
     public int numConnections(){
         return numberOfConnections;
     }
 
+    /**
+     * Removes a connection from this Node to another specified Node.
+     * @param gnode The other node to remove the connection with.
+     * @return true if connection was found and removed, false otherwise.
+     */
     public boolean removeConnection(GraphNode<T> gnode){
         if(connections == null){
             return false;
@@ -69,10 +134,14 @@ public class GraphNode<T> {
         return false;
     }
 
+    /**
+     * Visualization of Node Object.
+     * Prints the current value of the Node as well as the values of the nodes this node has connections with.
+     * @return String node
+     */
     @Override
     public String toString() {
         String ans = "Node: "+val+"\n";
-        ans += "Count: "+numberOfConnections+"\n";
         ans += "Connections: [";
         Node<GraphNode<T>> start = connections;
         while(start!=null){
@@ -85,6 +154,11 @@ public class GraphNode<T> {
         return ans + "]\n---------------";
     }
 
+    /**
+     * helper method to remove the connection from a given node to this node.
+     * Ensures that if we remove a connection from one node the connections is severed on both ends.
+     * @param gnode The node we want to remove this node from.
+     */
     private void cleanup(GraphNode<T> gnode){
         if(gnode.connections.val()==this){
             gnode.connections = gnode.connections.next();
@@ -101,6 +175,11 @@ public class GraphNode<T> {
         }
     }
 
+    /**
+     * Searches a linked List of This node's connections to see if this node already has a connection with the given node.
+     * @param node Node object we are searching for.
+     * @return true if connection already exists, false otherwise.
+     */
     private boolean searchAndFound(GraphNode<T> node){
         Node<GraphNode<T>> start = connections;
         while(start!=null){
@@ -110,22 +189,6 @@ public class GraphNode<T> {
             start = start.next();
         }
         return false;
-    }
-
-    public static void main(String[] args){
-        GraphNode<String> JFK = new GraphNode<>("JFK");
-        GraphNode<String> LAX = new GraphNode<>("LAX");
-        GraphNode<String> DAL = new GraphNode<>("DAL");
-        GraphNode<String> EWR = new GraphNode<>("EWR");
-        JFK.addConnection(LAX);
-        JFK.addConnection(DAL);
-        JFK.addConnection(EWR);
-
-        System.out.println(DAL.removeConnection(JFK));
-        System.out.println(JFK);
-        System.out.println(LAX);
-        System.out.println(DAL);
-        System.out.println(EWR);
     }
 
 }

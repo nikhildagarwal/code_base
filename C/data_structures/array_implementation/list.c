c185d5620e3f72f8b24c14cfb5a7f9eaca368947
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-06-06 18:57:16 -0400

    full list implementation in C

--- C/data_structures/array_implementation/list.c
+++ C/data_structures/array_implementation/list.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-const int INIT_CAPACITY = 4;
+const int INIT_CAPACITY = 10;
 const int INDEX_OUT_OF_BOUNDS_ERROR = 9;
 const int INIT_SIZE = 0;
 
@@ -24,13 +24,24 @@ void endList(List* l){
 
 void printList(List* l){
     printf("[");
-    for(int i = 0;i<l->length;i++){
+    for(int i = 0;i<l->size;i++){
         printf("%d",l->array[i]);
-        if(i!=l->length - 1){
+        if(i!=l->size - 1){
             printf(", ");
         }
     }
     printf("]\n");
+}
+
+void reverse(List* l){
+    int mid = l->size / 2;
+    int pR = l->size - 1;
+    for(int i =0;i<mid;i++){
+        int temp = l->array[i];
+        l->array[i] = l->array[pR];
+        l->array[pR] = temp;
+        pR--;
+    }
 }
 
 void add(List* l, int value){
@@ -40,6 +51,14 @@ void add(List* l, int value){
     }
     l->array[l->size] = value;
     l->size++;
+}
+
+void set(List* l, int index, int value){
+    if(index<0 || index >= l->size){
+        printf("Index Out Of Bounds Exception (9)");
+        exit(INDEX_OUT_OF_BOUNDS_ERROR);
+    }
+    l->array[index] = value;
 }
 
 int delete(List* l, int index){
@@ -63,12 +82,39 @@ int size(List* l){
     return l->size;
 }
 
+int isEmpty(List* l){
+    if(l->size == 0){
+        return 1;
+    }
+    return 0;
+}
+
+int contains(List* l, int value){
+    for(int i = 0;i<l->size;i++){
+        if(l->array[i] == value){
+            return 1;
+        }
+    }
+    return 0;
+}
+
+int get(List* l, int index){
+    if(index<0 || index >= l->size){
+        printf("Index Out Of Bounds Exception (9)");
+        exit(INDEX_OUT_OF_BOUNDS_ERROR);
+    }
+    return l->array[index];
+}
+
 int main(){
     List list1;
     initList(&list1);
     for(int i =0;i<7;i++){
         add(&list1,i);
     }
+    printList(&list1);
+    reverse(&list1);
+    printList(&list1);
     for(int i = 0;i<4;i++){
         printf("%d\n",delete(&list1,0));
     }

6b96e8a0e9831093290f132981f2f71665907747
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-06-07 15:36:49 -0400

    queue implementation in C (uses linked list)

---
+++ C/data_structures/node_implementation/queue.c
@@ -0,0 +1,129 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+const int INIT_SIZE = 0;
+const int EMPTY_QUEUE_ERROR = 7;
+
+struct Node{
+    int val;
+    struct Node* next;
+};
+
+typedef struct{
+    int size;
+    struct Node* front;
+    struct Node* end;
+} Queue;
+
+void initQueue(Queue* q){
+    q->size = INIT_SIZE;
+    q->front = NULL;
+    q->end = NULL;
+}
+
+void push(Queue* q, int value){
+    if(q->size == 0){
+        q->front = (struct Node*)malloc(sizeof(struct Node));
+        q->front->val = value;
+        q->end = q->front;
+        q->end->next = NULL;
+    }else{
+        struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+        newNode->val = value;
+        newNode->next = NULL;
+        q->end->next = newNode;
+        q->end = q->end->next;
+    }
+    q->size++;
+}
+
+void printQueue(Queue* q){
+    printf("out [");
+    struct Node* start = q->front;
+    while(start!=NULL){
+        printf("%d",start->val);
+        if(start->next != NULL){
+            printf(", ");
+        }
+        start = start->next;
+    }
+    printf("] in\n");
+}
+
+void endQueue(Queue* q){
+    struct Node* start = q->front;
+    while(start!=NULL){
+        struct Node* toFree = start;
+        start = start->next;
+        free(toFree);
+    }
+}
+
+int isEmpty(Queue* q){
+    if(q->size == 0){
+        return 1;
+    }
+    return 0;
+}
+
+int size(Queue* q){
+    return q->size;
+}
+
+int peek(Queue* q){
+    if(q->size == 0){
+        printf("Empty Queue Error");
+        exit(EMPTY_QUEUE_ERROR);
+    }
+    return q->front->val;
+}
+
+int poll(Queue* q){
+    if(q->size == 0){
+        printf("Empty Queue Error");
+        exit(EMPTY_QUEUE_ERROR);
+    }
+    q->size--;
+    int value = q->front->val;
+    struct Node* toFree = q->front;
+    q->front = q->front->next;
+    if(q->front == NULL){
+        q->end = NULL;
+    }
+    free(toFree);
+    return value;
+}
+
+int contains(Queue* q, int value){
+    struct Node* start = q->front;
+    while(start!=NULL){
+        if(start->val == value){
+            return 1;
+        }
+        start = start->next;
+    }
+    return 0;
+}
+
+int main(){
+    Queue queue1;
+    initQueue(&queue1);
+    push(&queue1,31);
+    push(&queue1,3);
+    push(&queue1,-43);
+    push(&queue1,-32);
+    push(&queue1,77);
+    printf("Size: %d\n",size(&queue1));
+    printQueue(&queue1);
+    printf("Peek: %d\n",peek(&queue1));
+    printf("Queue contains 31: %d\n",contains(&queue1,31));
+    printf("polled: %d\n",poll(&queue1));
+    printQueue(&queue1);
+    printf("Queue contains 31: %d\n",contains(&queue1,31));
+    printf("polled: %d\n",poll(&queue1));
+    printQueue(&queue1);
+    printf("polled: %d\n",poll(&queue1));
+    printQueue(&queue1);
+    endQueue(&queue1);
+    return 0;
+}

dde04a95475005c6eb4c22da8e3b6883443430d5
Nikhil Agarwal <email.nikhil.agarwal@gmail.com>
2023-07-14 01:52:49 -0400

    full implementation of map

--- C/data_structures/node_implementation/map.c
+++ C/data_structures/node_implementation/map.c
@@ -1,3 +1,22 @@
+/**
+ * Implementation of a Map data structure in C.
+ * Uses Node structures linked together into multiple singly linked list.
+ * Each Node contains a key and value of type integer.
+ * Uses an array of "buckets" where the contents of a each bucket is a linked list.
+ * By definition of a Map, each key must be unique.
+ * Automatically increases the number of buckets and redistributes nodes to the buckets after the ratio of elements to buckets has reached 0.75 (load factor of 0.75)
+ * Init as -->  Map m1;
+ *              initMap(&m1);
+ * Destroy -->  endMap(&m1);
+ * Methods -->  void put(Map* m, int key, int val);
+ *              int getValueFromKey(Map* m, int key);
+ *              int* getKeysFromValue(Map* m, int value);
+ *              int size(Map* m);
+ *              printMap_Testing(Map* m);
+ *              int removeKey(Map* m, int key);
+ * @author Nikhil Daehee Agarwal
+*/
+
 #include <stdlib.h>
 #include <stdio.h>
 
@@ -6,12 +25,19 @@ double INIT_LOAD = 0.0;
 int INIT_BUCKET_COUNT = 16;
 double LOAD_FACTOR = 0.75;
 
+/**
+ * Constructor for node in linked list
+ * contains int key and int val
+*/
 struct Node{
     int key;
     int val;
     struct Node* next;
 };
 
+/**
+ * Constructor for Map
+*/
 typedef struct{
     int size;
     double load;
@@ -19,6 +45,10 @@ typedef struct{
     struct Node** buckets;
 } Map;
 
+/**
+ * Initializes Map by allocating space for buckets and setting size and load to 0
+ * @param m : pointer to map
+*/
 void initMap(Map* m){
     m->size = INIT_SIZE;
     m->load = INIT_LOAD;
@@ -26,6 +56,71 @@ void initMap(Map* m){
     m->buckets = (struct Node**)malloc(INIT_BUCKET_COUNT * sizeof(struct Node*));
 }
 
+/**
+ * End method for Map
+ * Frees all previously allocated memory
+ * @param m : pointer to map
+*/
+void endMap(Map* m){
+    for(int i = 0;i<m->bucket_count;i++){
+        struct Node* head = m->buckets[i];
+        while(head != NULL){
+            struct Node* toFree = head;
+            head = head->next;
+            free(toFree);
+        }
+    }
+    free(m->buckets);
+}
+
+/**
+ * Checks to see if map contains a certain key
+ * @param m : pointer to map
+ * @param key : int key
+ * @return 1 if key is found, 0 otherwise
+*/
+int containsKey(Map* m, int key){
+    int index = key % m->bucket_count;
+    if(index<0){
+        index *= -1;
+    }
+    struct Node* head = m->buckets[index];
+    while(head!=NULL){
+        if(head->key == key){
+            return 1;
+        }
+        head = head->next;
+    }
+    return 0;
+}
+
+/**
+ * Checks to see if map contains a certain value
+ * @param m : pointer to map
+ * @param value : int value
+ * @return 1 if value is found, 0 otherwise
+*/
+int containsValue(Map* m, int value){
+    for(int i = 0;i<m->bucket_count;i++){
+        struct Node* head = m->buckets[i];
+        while(head != NULL){
+            if(head->val == value){
+                return 1;
+            }
+            head = head->next;
+        }
+    }
+    return 0;
+}
+
+/**
+ * adds a key, value pair to the map
+ * if the key already exists in the map, the value is updated.
+ * rearranges nodes into double number of buckets once a load factor of 0.75 has been reached
+ * @param m : Pointer to map
+ * @param key : int key
+ * @param val : int value
+*/
 void put(Map* m, int key, int val){
     int index = key % m->bucket_count;
     if(index < 0){
@@ -84,6 +179,12 @@ void put(Map* m, int key, int val){
     m->load = noe / nob;
 }
 
+/**
+ * Method to get the value associated with a specific key
+ * @param m : pointer to map
+ * @param key : int key we want to search for
+ * @return integer value of key
+*/
 int getValueFromKey(Map* m, int key){
     int index = key % m->bucket_count;
     if(index < 0){
@@ -99,6 +200,16 @@ int getValueFromKey(Map* m, int key){
     return NULL;
 }
 
+/**
+ * getter method to return int pointer (Array) of all keys that are associated with a given value
+ * @param m : pointer to map
+ * @param value : value we want to search map for
+ * @return int* of all keys that have value as specified,
+ *         index 0 of the pointer will contain the number of keys found with that value.
+ *         Ex: index 0 of int* could hold 0 (indication no keys found with given value) or 5 (indicating 5 keys found),
+ *         the zero index value allows us to loop across the pointer,
+ *         *** REMEMBER TO FREE ***
+*/
 int* getKeysFromValue(Map* m, int value){
     int* array = (int*)malloc(sizeof(int));
     array[0] = 0;
@@ -116,6 +227,11 @@ int* getKeysFromValue(Map* m, int value){
     return array;
 }
 
+/**
+ * Prints map to the command line, for testing purposes
+ * Displays buckets and corresponding linked lists
+ * @param m : pointer to map
+*/
 void printMap_Testing(Map* m){
     printf("size: %d, load: %f\n",m->size,m->load);
     for(int i = 0;i<m->bucket_count;i++){
@@ -130,10 +246,22 @@ void printMap_Testing(Map* m){
     printf("\n");
 }
 
+/**
+ * gets the number of elements currently in the map (size of map)
+ * @param m : pointer to Map
+ * @return int size
+*/
 int size(Map* m){
     return m->size;
 }
 
+/**
+ * Removes key, value pair from map
+ * if the key is found, the pair is removed, else, this method returns prematurely without terminating due to error.
+ * @param m : pointer to map
+ * @param key : int key search for and remove
+ * @return 0 if key is not found and not removed, 1 if found and removed
+*/
 int removeKey(Map* m, int key){
     int index = key % m->bucket_count;
     if(index < 0){
